b1037cc5f85830bb68a4f11c8e93927ad137fa99
funkybooboo <73196352+funkybooboo@users.noreply.github.com>
2022-05-22 18:17:01 -0600

    Add files via upload

---
+++ LifeSimulator.java
@@ -0,0 +1,25 @@
+public class LifeSimulator {
+    public LifeSimulator(int sizeX, int sizeY) {
+
+    }
+
+    public int getSizeX() {
+
+    }
+
+    public int getSizeY() {
+
+    }
+
+    public boolean getCell(int x, int y) {
+
+    }
+
+    public void insertPattern(Pattern pattern, int startX, int startY) {
+
+    }
+
+    public void update() {
+
+    }
+}

b865517d7317eee846129d2faab4680db3eb6581
funkybooboo <nathanstott01@gmail.com>
2023-03-25 23:07:39 -0600

    finished

--- src/UnionFind.java
+++ src/UnionFind.java
@@ -1,8 +1,9 @@
 import java.util.Arrays;
+import java.util.Random;
 
 public class UnionFind {
 
-    // item : index | group : stored or -size
+    // item : index | group : stored or -size if root
     // union-by-size
 
     int[] groups;
@@ -14,24 +15,32 @@ public class UnionFind {
         }
     }
 
-    public void union(int a, int b) {
-        int groupA = find(a);
-        int groupB = find(b);
-        if (groups[groupA] <= groups[groupB]) {
-            groups[groupB] = groupA;
-            groups[groupA] -= 1;
+    public void union(int indexA, int indexB) {
+        int groupIndexA = find(indexA);
+        int groupIndexB = find(indexB);
+        if (groupIndexA == groupIndexB) return;
+        if (groups[groupIndexA] <= groups[groupIndexB]) {
+            groups[groupIndexB] = groupIndexA;
+            groups[groupIndexA] -= 1;
         } else {
-            groups[groupA] = groupB;
-            groups[groupB] -= 1;
+            groups[groupIndexA] = groupIndexB;
+            groups[groupIndexB] -= 1;
         }
     }
 
-    public int find(int a) {
-        if (groups[a] < 0) {
-            return a;
+    public int find(int indexA) {
+        if (groups[indexA] < 0) {
+            return indexA;
         }
-        groups[a] = find(groups[a]);
-        return groups[a];
+        groups[indexA] = find(groups[indexA]);
+        return groups[indexA];
+    }
+
+    public boolean isSameGroup(int indexA, int indexB) {
+        if (groups[indexA] < 0 || groups[indexB] < 0) return false;
+        int groupIndexA = find(indexA);
+        int groupIndexB = find(indexB);
+        return groupIndexA == groupIndexB;
     }
 
     @Override
@@ -45,17 +54,21 @@ public class UnionFind {
     }
 
     public static void main(String[] args) {
-        int[] data = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int size = 30;
+        int[] data = new int[size];
+        for (int i = 0; i < size; i++) {
+            data[i] = i;
+        }
         UnionFind unionFind = new UnionFind(data);
-        unionFind.union(0, 1);
-        unionFind.union(0, 2);
-        unionFind.union(4, 5);
-        unionFind.union(2, 4);
-        unionFind.union(1, 7);
-        unionFind.union(8, 6);
-        unionFind.union(3, 6);
         System.out.println(unionFind);
-
+        Random random = new Random();
+        for (int i = 0; i < size/2; i++) {
+            int a = random.nextInt(size);
+            int b = random.nextInt(size);
+            System.out.println("Union("+a+", "+b+")");
+            unionFind.union(a, b);
+            System.out.println(unionFind);
+        }
     }
 
 }

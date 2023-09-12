9dae5b5d023f7ea06efa8d0d32af85cf69d80cde
Nate Stott <nathanstott01@gmail.com>
2023-09-12 08:23:32 -0600

    fixed

--- Main.java
+++ Main.java
@@ -1,14 +1,14 @@
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
         int K = 20;
-        int[] items = {9, 3, 24, 14, 5, 8, 17, 4};
+        //int[] items = {9, 3, 24, 14, 5, 8, 17, 4};
         //int[] items = {5, 1, 5, 1, 5, 1};
+        int[] items = {1, 50, 7, 50, 50, 2};
 
         // n1 + n2 ... nn >= K/2 and n1 + n2 ... nn < K
 
@@ -27,7 +27,6 @@ public class Main {
             solution.add(item1);
             total += item1;
 
-
             for (int j = 1; j < items.length; j++) {
                 int item2 = items[j];
                 if (item2 > K) continue;
@@ -36,7 +35,7 @@ public class Main {
                 solution.add(item2);
                 total += item2;
 
-                if (total > K) break;
+                if (total > K) total -= item2;
                 if (total >= K / 2) return solution.stream().mapToInt(Integer::intValue).toArray();
             }
         }

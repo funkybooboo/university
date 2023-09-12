4f5b63ffcb077504d98fe4e53d34e7442a357237
Nate Stott <nathanstott01@gmail.com>
2023-09-12 08:04:42 -0600

    finished algorithm

--- Main.java
+++ Main.java
@@ -7,8 +7,8 @@ public class Main {
     public static void main(String[] args) {
 
         int K = 20;
-        //int[] items = {9, 3, 24, 14, 5, 8, 17, 4};
-        int[] items = {5, 1, 5, 1, 5, 1};
+        int[] items = {9, 3, 24, 14, 5, 8, 17, 4};
+        //int[] items = {5, 1, 5, 1, 5, 1};
 
         // n1 + n2 ... nn >= K/2 and n1 + n2 ... nn < K
 
@@ -17,24 +17,27 @@ public class Main {
 
     // O(n^2)
     public static int[] slowSolve(int K, int[] items) {
-        for (int i = 0; i < items.length; i++) {
-            List<Integer> solution = new ArrayList<>();
-            int item1 = items[i];
+        for (int item1 : items) {
             if (item1 > K) continue;
-            if (item1 >= K/2) return new int[]{item1};
+            if (item1 >= K / 2) return new int[]{item1};
+
+            List<Integer> solution = new ArrayList<>();
+            int total = 0;
+
             solution.add(item1);
+            total += item1;
+
+
             for (int j = 1; j < items.length; j++) {
                 int item2 = items[j];
                 if (item2 > K) continue;
-                if (item2 >= K/2) return new int[]{item2};
-                solution.add(item2);
+                if (item2 >= K / 2) return new int[]{item2};
 
-                int total = 0;
-                for (int n : solution) {
-                    total += n;
-                }
-                if (total > K) continue;
-                if (total >= K/2) return solution.stream().mapToInt(Integer::intValue).toArray();
+                solution.add(item2);
+                total += item2;
+
+                if (total > K) break;
+                if (total >= K / 2) return solution.stream().mapToInt(Integer::intValue).toArray();
             }
         }
         return null;

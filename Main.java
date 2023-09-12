97b88a86f34048782e0b24dab1aafd8893d7cc5b
Nate Stott <nathanstott01@gmail.com>
2023-09-12 08:27:02 -0600

    got program to work with doubles

--- Main.java
+++ Main.java
@@ -5,10 +5,10 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-        int K = 20;
+        double K = 20;
         //int[] items = {9, 3, 24, 14, 5, 8, 17, 4};
         //int[] items = {5, 1, 5, 1, 5, 1};
-        int[] items = {1, 50, 7, 50, 50, 2};
+        double[] items = {1, 50, 7, 50, 50, 2};
 
         // n1 + n2 ... nn >= K/2 and n1 + n2 ... nn < K
 
@@ -16,27 +16,27 @@ public class Main {
     }
 
     // O(n^2)
-    public static int[] slowSolve(int K, int[] items) {
-        for (int item1 : items) {
+    public static double[] slowSolve(double K, double[] items) {
+        for (double item1 : items) {
             if (item1 > K) continue;
-            if (item1 >= K / 2) return new int[]{item1};
+            if (item1 >= K / 2) return new double[]{item1};
 
-            List<Integer> solution = new ArrayList<>();
-            int total = 0;
+            List<Double> solution = new ArrayList<>();
+            double total = 0;
 
             solution.add(item1);
             total += item1;
 
             for (int j = 1; j < items.length; j++) {
-                int item2 = items[j];
+                double item2 = items[j];
                 if (item2 > K) continue;
-                if (item2 >= K / 2) return new int[]{item2};
+                if (item2 >= K / 2) return new double[]{item2};
 
                 solution.add(item2);
                 total += item2;
 
                 if (total > K) total -= item2;
-                if (total >= K / 2) return solution.stream().mapToInt(Integer::intValue).toArray();
+                if (total >= K / 2) return solution.stream().mapToDouble(Double::doubleValue).toArray();
             }
         }
         return null;

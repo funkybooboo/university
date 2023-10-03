41b01d4983d23042589ad543efe07fb1b9948d94
Nate Stott <nathanstott01@gmail.com>
2023-10-03 10:12:58 -0600

    start the assignment

---
+++ castle.pl
@@ -0,0 +1,15 @@
+% Base case, empty list, do nothing
+printList([]).
+printList(H | T) :-
+    write(H), nl,
+    printList(T).
+
+% Add your comments here
+solveRooms(Castle, L) :- 
+  printList(NL).
+
+
+% Add your comments here
+solveRoomsWithinCost(Castle, Limit) :- 
+  printList(NL).
+

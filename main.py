a1254cd7acfde22b4d70f2b126601b0a5f1f4e97
Nate Stott <nate.stott@proton.me>
2023-10-16 16:11:10 -0600

    got other work

--- main.py
+++ main.py
@@ -11,6 +11,36 @@ def main():
     print(k)
     print(get_Kth(A, k))
 
+def find_peak(array):
+    left = 0
+    right = len(array) - 1
+    while left < right:
+        mid = (left + right) // 2
+        if array[mid] < array[mid + 1]:
+            left = mid + 1
+        elif array[mid] > array[mid + 1]:
+            right = mid
+    return array[left]
+
+
+def fit_line(array):
+    x_mean = 0
+    y_mean = 0
+    for p in array:
+        x_mean += p[0]
+        y_mean += p[1]
+    x_mean /= len(array)
+    y_mean /= len(array)
+    temp1 = 0
+    temp2 = 0
+    for p in array:
+        temp1 += (p[0] - x_mean) * (p[1] - y_mean)
+        temp2 += (p[0] - x_mean) ** 2
+    m = temp1 / temp2
+    b = y_mean - (m * x_mean)
+
+    return m, b
+
 def get_Kth(A, k):
     if k > len(A):
         return None

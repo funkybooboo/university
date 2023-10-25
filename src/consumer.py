b31c32eb9dd4985fac6dfe7577aeeb2b365e05a8
Nate Stott <nate.stott@proton.me>
2023-10-25 11:26:29 -0600

    cleaned up code

--- src/consumer.py
+++ src/consumer.py
@@ -4,27 +4,50 @@ import json
 import logging
 import argparse
 
+
+def get_args():
+    parser = argparse.ArgumentParser(description='Pulls data from a AWS S3 bucket and pushes it to another bucket or DynamoDB table')
+    parser.add_argument('--pull-choice', type=str, default='sqs', help='Select pull choice (default=s3)')
+    parser.add_argument('--push-choice', type=str, default='s3', help='Select push choice (default=s3)')
+    parser.add_argument('-r', '--region', type=str, default='us-east-1', help='Name of AWS region used (default=us-east-1)')
+    parser.add_argument('--pull-bucket', type=str, default='usu-cs5260-nate-requests', help='Name of bucket that will contain requests (default=usu-cs5260-nate-requests)')
+    parser.add_argument('--pull-queue', type=str, default='cs5260-requests', help='Name of queue that will contain requests (default=cs5260-requests)')
+    parser.add_argument('--push-bucket', type=str, default='usu-cs5260-nate-web', help='Name of bucket where widget info will be pushed (default=usu-cs5260-nate-web)')
+    parser.add_argument('--push-table', type=str, default='widgets', help='Name of table where widget info will be pushed (default=widgets)')
+    parser.add_argument('-mrt', '--max-runtime', type=int, default=1000, help='Maximum runtime in seconds (default=1000)')
+    parser.add_argument('-mwr', '--max-widget-pulls', type=int, default=500, help='Maximum number of widget requests to pull (default=500)')
+    parser.add_argument('-ipd', '--inter-pull-delay', type=int, default=100, help='Number of milliseconds to wait between request pulls (default=100)')
+    parser.add_argument('-ll', '--log-level', type=bool, default=False, help='Level of logging (default=low)')
+    return vars(parser.parse_args())
+
+
+args = get_args()
 message_cache = []
+s3_client = boto3.client('s3')
+sts = boto3.client('sts')
+sqs = boto3.client('sqs', region_name=args['region'])
+dynamodb = boto3.resource('dynamodb', region_name=args['region'])
+table = dynamodb.Table(args['push_table'])
+
 
 def main():
-    args = get_args()
-    setup_logging(args)
+    setup_logging()
     logging.info('start program execution')
     if args['pull_choice'] == 's3' and args['push_choice'] == 's3':
-        consume(args, pull_widget_s3, push_widget_s3)
+        consume(pull_widget_s3, push_widget_s3)
     elif args['pull_choice'] == 's3' and args['push_choice'] == 'dynamodb':
-        consume(args, pull_widget_s3, push_widget_dynamodb)
+        consume(pull_widget_s3, push_widget_dynamodb)
     elif args['pull_choice'] == 'sqs' and args['push_choice'] == 's3':
-        consume(args, pull_widget_sqs, push_widget_s3)
+        consume(pull_widget_sqs, push_widget_s3)
     elif args['pull_choice'] == 'sqs' and args['push_choice'] == 'dynamodb':
-        consume(args, pull_widget_sqs, push_widget_dynamodb)
+        consume(pull_widget_sqs, push_widget_dynamodb)
     else:
         logging.error('invalid push or pull options')
     logging.info('end program execution')
     logging.shutdown()
 
 
-def setup_logging(args):
+def setup_logging():
     log_level = logging.DEBUG if args['log_level'] else logging.INFO
     logging.basicConfig(
         level=log_level,
@@ -36,46 +59,47 @@ def setup_logging(args):
     )
 
 
-def consume(args, pull, push):
+def consume(pull, push):
     end = (time.time() * 1000) + (args['max_runtime'] * 1000)
     count = 0
     while (time.time() * 1000) < end and count < args['max_widget_pulls']:
-        widget, widget_key = pull(args)
+        widget, widget_key = pull()
         count += 1
         if widget is None:
             logging.info('no widget')
             time.sleep((args['inter_pull_delay'] / 1000))
             continue
+        if widget_key == 'unknown':
+            widget_key = widget['requestId']
         logging.info(f'widget: {widget_key}')
-        push(widget, widget_key, args)
+        push(widget, widget_key)
 
 
-def push_widget_s3(widget, widget_key, args):
-    push_widget(widget, widget_key, args, create_s3_object, update_s3_object, delete_s3_object)
+def push_widget_s3(widget, widget_key):
+    push_widget(widget, widget_key, create_s3_object, update_s3_object, delete_s3_object)
 
 
-def push_widget_dynamodb(widget, widget_key, args):
-    push_widget(widget, widget_key, args, create_dynamodb_object, update_dynamodb_object, delete_dynamodb_object)
+def push_widget_dynamodb(widget, widget_key):
+    push_widget(widget, widget_key, create_dynamodb_object, update_dynamodb_object, delete_dynamodb_object)
 
 
-def push_widget(widget, widget_key, args, create_object, update_object, delete_object):
+def push_widget(widget, widget_key, create_object, update_object, delete_object):
     if widget['type'] == 'create' and is_valid_create(widget):
         logging.info('processing valid create widget')
-        create_object(widget, widget_key, args)
+        create_object(widget, widget_key)
     elif widget['type'] == 'update' and is_valid_update(widget):
         logging.info('processing valid update widget')
-        update_object(widget, widget_key, args)
+        update_object(widget, widget_key)
     elif widget['type'] == 'delete' and is_valid_delete(widget):
         logging.info('processing valid delete widget')
-        delete_object(widget, widget_key, args)
+        delete_object(widget, widget_key)
     else:
         logging.error(f'invalid widget type: {widget_key}')
 
 
-def create_s3_object(widget, widget_key, args):
+def create_s3_object(widget, widget_key):
     logging.info('attempt create widget s3')
     try:
-        s3_client = boto3.client('s3')
         object_key = get_s3_object_key(widget)
         item = {
             'id': widget['widgetId'],
@@ -92,11 +116,9 @@ def create_s3_object(widget, widget_key, args):
         logging.error(e)
 
 
-def create_dynamodb_object(widget, widget_key, args):
+def create_dynamodb_object(widget, widget_key):
     logging.info('attempt create widget dynamodb')
     try:
-        dynamodb = boto3.resource('dynamodb', region_name=args['region'])
-        table = dynamodb.Table(args['push_table'])
         item = {
             'id': widget['widgetId'],
             'owner': widget['owner'],
@@ -112,11 +134,10 @@ def create_dynamodb_object(widget, widget_key, args):
         logging.error(e)
 
 
-def update_s3_object(widget, widget_key, args):
+def update_s3_object(widget, widget_key):
     logging.info('attempt update widget s3')
     try:
         object_key = get_s3_object_key(widget)
-        s3_client = boto3.client('s3')
         response = s3_client.get_object(Bucket=args['push_bucket'], Key=object_key)
         object_data = response['Body'].read()
         s3_client.delete_object(Bucket=args['push_bucket'], Key=object_key)
@@ -131,11 +152,9 @@ def update_s3_object(widget, widget_key, args):
         logging.error(e)
 
 
-def update_dynamodb_object(widget, widget_key, args):
+def update_dynamodb_object(widget, widget_key):
     logging.info('attempt update widget dynamodb')
     try:
-        dynamodb = boto3.resource('dynamodb', region_name=args['region'])
-        table = dynamodb.Table(args['push_table'])
         key = {"id": widget["widgetId"]}
         response = table.get_item(Key=key)
         if 'Item' in response:
@@ -162,11 +181,10 @@ def update_item(old_widget, widget):
     return item
 
 
-def delete_s3_object(widget, widget_key, args):
+def delete_s3_object(widget, widget_key):
     logging.info('attempt delete widget s3')
     try:
         object_key = get_s3_object_key(widget)
-        s3_client = boto3.client('s3')
         s3_client.delete_object(Bucket=args['push_bucket'], Key=object_key)
         logging.info(f'success delete widget s3: {widget_key}')
     except Exception as e:
@@ -174,11 +192,9 @@ def delete_s3_object(widget, widget_key, args):
         logging.error(e)
 
 
-def delete_dynamodb_object(widget, widget_key, args):
+def delete_dynamodb_object(widget, widget_key):
     logging.info('attempt delete widget dynamodb')
     try:
-        dynamodb = boto3.resource('dynamodb', region_name=args['region'])
-        table = dynamodb.Table(args['push_table'])
         key = {'id': widget['widgetId']}
         table.delete_item(Key=key)
         logging.info(f'success delete widget dynamodb: {widget_key}')
@@ -218,11 +234,11 @@ def is_valid_delete(widget):
     return False
 
 
-def pull_widget_sqs(args):
+def pull_widget_sqs():
     logging.info('attempt widget pull sqs')
     widget_key = 'unknown'
     try:
-        queue_url, sqs = get_queue_url(args)
+        queue_url = get_queue_url()
         if len(message_cache) == 0:
             response = sqs.receive_message(
                 QueueUrl=queue_url,
@@ -243,7 +259,9 @@ def pull_widget_sqs(args):
                     sqs.delete_message(QueueUrl=queue_url, ReceiptHandle=receipt_handle)
         if len(message_cache) > 0:
             logging.info('success read got widget')
-            return process_message(message_cache.pop(0), widget_key)
+            message = message_cache.pop(0)
+            widget_key = message['MessageId']
+            return json.loads(message['Body']), widget_key
         logging.info('success read no widget')
     except Exception as e:
         logging.error(f'fail read: {widget_key}')
@@ -251,28 +269,17 @@ def pull_widget_sqs(args):
     return None, None
 
 
-def process_message(message, widget_key):
-    try:
-        widget_key = json.loads(message['Key'])
-    except KeyError:
-        logging.error("No 'widget_key' found in the message body.")
-    logging.info('success read got widget')
-    return json.loads(message['Body']), widget_key
-
-def get_queue_url(args):
+def get_queue_url():
     region = args['region']
-    sts = boto3.client('sts')
     response = sts.get_caller_identity()
     account_number = response['Account']
     queue = args['pull_queue']
-    sqs = boto3.client('sqs', region_name=region)
     queue_url = f'https://sqs.{region}.amazonaws.com/{account_number}/{queue}'
-    return queue_url, sqs
+    return queue_url
 
 
-def pull_widget_s3(args):
+def pull_widget_s3():
     logging.info('attempt widget pull s3')
-    s3_client = boto3.client('s3')
     widget_key = 'unknown'
     try:
         response = s3_client.list_objects_v2(Bucket=args['pull_bucket'])
@@ -291,22 +298,6 @@ def pull_widget_s3(args):
         logging.error(f'fail read: {widget_key}')
         logging.error(e)
     return None, None
-
-
-def get_args():
-    parser = argparse.ArgumentParser(description='Pulls data from a AWS S3 bucket and pushes it to another bucket or DynamoDB table')
-    parser.add_argument('--pull-choice', type=str, default='s3', help='Select pull choice (default=s3)')
-    parser.add_argument('--push-choice', type=str, default='s3', help='Select push choice (default=s3)')
-    parser.add_argument('-r', '--region', type=str, default='us-east-1', help='Name of AWS region used (default=us-east-1)')
-    parser.add_argument('--pull-bucket', type=str, default='usu-cs5260-nate-requests', help='Name of bucket that will contain requests (default=usu-cs5260-nate-requests)')
-    parser.add_argument('--pull-queue', type=str, default='cs5260-requests', help='Name of queue that will contain requests (default=cs5260-requests)')
-    parser.add_argument('--push-bucket', type=str, default='usu-cs5260-nate-web', help='Name of bucket where widget info will be pushed (default=usu-cs5260-nate-web)')
-    parser.add_argument('--push-table', type=str, default='widgets', help='Name of table where widget info will be pushed (default=widgets)')
-    parser.add_argument('-mrt', '--max-runtime', type=int, default=1000, help='Maximum runtime in seconds (default=1000)')
-    parser.add_argument('-mwr', '--max-widget-pulls', type=int, default=500, help='Maximum number of widget requests to pull (default=500)')
-    parser.add_argument('-ipd', '--inter-pull-delay', type=int, default=100, help='Number of milliseconds to wait between request pulls (default=100)')
-    parser.add_argument('-ll', '--log-level', type=bool, default=False, help='Level of logging (default=low)')
-    return vars(parser.parse_args())
 
 
 if __name__ == '__main__':

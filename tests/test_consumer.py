34806b6377cb97660254354b216e05334707bea5
Nate Stott <nathanstott01@gmail.com>
2023-11-06 11:26:12 -0700

    finished unit test

--- tests/test_consumer.py
+++ tests/test_consumer.py
@@ -1,6 +1,6 @@
 import unittest
-from unittest.mock import patch, MagicMock
-from src.consumer import Session, create_s3_object, create_dynamodb_object, update_s3_object, update_dynamodb_object, delete_s3_object, delete_dynamodb_object, pull_widget_s3, pull_widget_sqs
+from unittest.mock import MagicMock
+from src.consumer import create_s3_object, create_dynamodb_object, update_s3_object, update_dynamodb_object, delete_s3_object, delete_dynamodb_object, pull_widget_s3, pull_widget_sqs
 
 
 class TestConsumerFunctions(unittest.TestCase):
@@ -16,63 +16,104 @@ class TestConsumerFunctions(unittest.TestCase):
         }
         widget_key = 'test'
         create_s3_object(widget, widget_key, session_mock)
+        session_mock.s3_client.put_object.assert_called_once()
 
-    @patch('boto3.Session')
-    def test_create_dynamodb_object(self, mock):
+
+    def test_create_dynamodb_object(self):
+        session_mock = MagicMock()
         widget = {
             'widgetId': '1234',
             'owner': 'test',
             'label': 'test_label',
-            'description': 'hello'
+            'description': 'hello',
+            'otherAttributes': []
         }
         widget_key = 'test'
-        create_dynamodb_object(widget, widget_key)
+        create_dynamodb_object(widget, widget_key, session_mock)
+        session_mock.table.put_item.assert_called_once()
 
-    @patch('boto3.Session')
-    def test_update_s3_object(self, mock):
+
+    def test_update_s3_object(self):
+        session_mock = MagicMock()
+        mock_response_stream = MagicMock()
+        mock_response_stream.read.return_value = b'{\'widgetId\': \'1234\', \'owner\': \'test\' , \'description\': \'old description\', \'label\': \'old label\', \'otherAttributes\': []}'
+        session_mock.s3_client.get_object.return_value = {'Body': mock_response_stream}
         widget = {
             'widgetId': '1234',
             'owner': 'test',
-            'description': 'hello'
+            'description': 'new description',
+            'otherAttributes': []
         }
         widget_key = 'test'
-        update_s3_object(widget, widget_key)
+        update_s3_object(widget, widget_key, session_mock)
+        session_mock.s3_client.get_object.assert_called_once()
+        session_mock.s3_client.delete_object.assert_called_once()
+        session_mock.s3_client.put_object.assert_called_once()
 
-    @patch('boto3.Session')
-    def test_update_dynamodb_object(self, mock):
+
+    def test_update_dynamodb_object(self):
+        session_mock = MagicMock()
+        session_mock.table.get_item.return_value = {'Item': {'widgetId': '1234', 'owner': 'test' , 'description': 'old description', 'label': 'old label', 'otherAttributes': []}}
         widget = {
             'widgetId': '1234',
             'owner': 'test',
-            'description': 'hello'
+            'description': 'hello',
+            'otherAttributes': []
         }
         widget_key = 'test'
-        update_dynamodb_object(widget, widget_key)
+        update_dynamodb_object(widget, widget_key, session_mock)
+        session_mock.table.get_item.assert_called_once()
+        session_mock.table.delete_item.assert_called_once()
+        session_mock.table.put_item.assert_called_once()
 
-    @patch('boto3.Session')
-    def test_delete_s3_object(self, mock):
+
+    def test_delete_s3_object(self):
+        session_mock = MagicMock()
         widget = {
             'widgetId': '1234',
             'owner': 'test',
         }
         widget_key = 'test'
-        delete_s3_object(widget, widget_key)
+        delete_s3_object(widget, widget_key, session_mock)
+        session_mock.s3_client.delete_object.assert_called_once()
 
-    @patch('boto3.Session')
-    def test_delete_dynamodb_object(self, mock):
+
+
+    def test_delete_dynamodb_object(self):
+        session_mock = MagicMock()
         widget = {
             'widgetId': '1234',
             'owner': 'test',
         }
         widget_key = 'test'
-        delete_dynamodb_object(widget, widget_key)
+        delete_dynamodb_object(widget, widget_key, session_mock)
+        session_mock.table.delete_item.assert_called_once()
 
-    @patch('boto3.Session')
-    def test_pull_widget_s3(self, mock):
-        widget, widget_key = pull_widget_s3()
 
-    @patch('boto3.Session')
-    def test_pull_widget_sqs(self, mock):
-        widget, widget_key = pull_widget_sqs()
+    def test_pull_widget_s3(self):
+        session_mock = MagicMock()
+        session_mock.s3_client.list_objects_v2.return_value = {'Contents': [{'Key': '1'}, {'Key': '2'}, {'Key': '3'}]}
+        mock_response_stream = MagicMock()
+        mock_response_stream.read.return_value = b'{}'
+        session_mock.s3_client.get_object.return_value = {'Body': mock_response_stream}
+        widget, widget_key = pull_widget_s3(session_mock)
+        self.assertEquals(widget, {})
+        self.assertEquals(widget_key, '1')
+        session_mock.s3_client.list_objects_v2.assert_called_once()
+        session_mock.s3_client.get_object.assert_called_once()
+        session_mock.s3_client.delete_object.assert_called_once()
+
+
+
+    def test_pull_widget_sqs(self):
+        session_mock = MagicMock()
+        session_mock.message_cache = []
+        session_mock.sqs.receive_message.return_value = {'Messages' : [{'ReceiptHandle': MagicMock(), 'MessageId': '1', 'Body': '{}'}]}
+        widget, widget_key = pull_widget_sqs(session_mock)
+        self.assertEquals(widget, {})
+        self.assertEquals(widget_key, '1')
+        session_mock.sqs.receive_message.assert_called_once()
+        session_mock.sqs.delete_message.assert_called_once()
 
 
 if __name__ == '__main__':

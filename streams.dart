a56849a5596683fd289f188e2838edaf810f5493
Nate Stott <nate.stott@proton.me>
2023-11-06 17:39:09 -0700

    finished deepen

--- streams.dart
+++ streams.dart
@@ -1,42 +1,41 @@
-List<int?>? flatten(List<List<int?>?>? list) {
+List<int?>? flatten(List<List<int?>?>? list) =>
+    list?.where((innerList) => innerList != null)
+    .expand((innerList) => innerList!.where((element) => element != null))
+    .toList();
+
+
+List<dynamic>? deepen(List<int?>? list) {
   if (list == null) return null;
   if (list.isEmpty) return [];
-  List<int?> result = [];
-  for (var l in list) {
-    for (var x in l!) {
-      result.add(x);
-    }
-  }
- return result;
+  if (list.length == 1) return list;
+  return deepenHelper(list.firstOrNull, list.sublist(1));
+}
+List<dynamic>? deepenHelper(int? h, List<int?> t) {
+  if (t.length == 1) return [h, [t.firstOrNull]];
+  return [h, deepenHelper(t.firstOrNull, t.sublist(1))];
 }
 
-List<List<int?>?>? deepen(List<int?>? list) {
-  if (list == null) return null;
-  if (list.isEmpty) return [];
-  List<int?> result = [];
-}
-
-Stream<int> Function() fibonnaciNumbers(int n) {
-
-}
-
-Stream<int> Function() streamFilter(Stream<int> stream, bool Function(int) f) {
-
-}
-
-Stream<int> Function() streamAccumulation(Stream<int> stream, int Function(int, int) f, initial) {
-
-}
-
-Stream<int> Function() generateNumbers(int n) {
-  final Stream<int> Function() func = (() async* {
-    for (int i = 1; i <= n; i++) {
-      await Future<void>.delayed(const Duration(seconds: 1));
-      yield i;
-    }
-  });
-  return func;
-}
+// Stream<int> Function() fibonnaciNumbers(int n) {
+//
+// }
+//
+// Stream<int> Function() streamFilter(Stream<int> stream, bool Function(int) f) {
+//
+// }
+//
+// Stream<int> Function() streamAccumulation(Stream<int> stream, int Function(int, int) f, initial) {
+//
+// }
+//
+// Stream<int> Function() generateNumbers(int n) {
+//   final Stream<int> Function() func = (() async* {
+//     for (int i = 1; i <= n; i++) {
+//       await Future<void>.delayed(const Duration(seconds: 1));
+//       yield i;
+//     }
+//   });
+//   return func;
+// }
 
 void main(List<String> arguments) async {
   print( 'flattening [[0,1], [2]] yields ${flatten([[0,1], [2]])}');
@@ -49,16 +48,16 @@ void main(List<String> arguments) async {
   print( 'deepening [0] yields ${deepen([0])}');
   print( 'deepening [] yields ${deepen([])}');
   print( 'deepening null yields ${deepen(null)}');
-  await for (final number in fibonnaciNumbers(7)()) {
-    print ('fibonnaci number is ${number}');
-  }
-  await for (final number in streamFilter(generateNumbers(10)(), ((a) {return a % 2 == 0;}))()) {
-    print ('filtered number is $number');
-  }
-
-  await for (final number in streamAccumulation(generateNumbers(10)(),((a,b) {return a+b;}),0)()) {
-    print ('cumulative number is $number.');
-  }
+  // await for (final number in fibonnaciNumbers(7)()) {
+  //   print ('fibonnaci number is ${number}');
+  // }
+  // await for (final number in streamFilter(generateNumbers(10)(), ((a) {return a % 2 == 0;}))()) {
+  //   print ('filtered number is $number');
+  // }
+  //
+  // await for (final number in streamAccumulation(generateNumbers(10)(),((a,b) {return a+b;}),0)()) {
+  //   print ('cumulative number is $number.');
+  // }
 }
 
 

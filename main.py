53014fe777006683e3271c5aa3422202cf713475
Nate Stott <nathanstott01@gmail.com>
2024-01-18 07:09:46 -0700

    cleaned up a bit

--- main.py
+++ main.py
@@ -14,13 +14,12 @@ def get_differance_table(function, left=0, right=10, down=10):
     for n in range(left, right):
         differance_table[0].append(n)
         differance_table[1].append(function(n))
-
     for i in range(2, down):
         differance_table.append([])
         for j in range(0, len(differance_table[i - 1]) - 1):
             differance_table[i].append(differance_table[i - 1][j + 1] - differance_table[i - 1][j])
-
     return differance_table
+
 
 def print_differance_table(differance_table):
     max_len = max(max(max(max(len(str(item)) for row in differance_table for item in row), len("n")), len("f(n)")), len(f"Δ^({len(differance_table) + 1})f(n)"))
@@ -32,9 +31,9 @@ def print_differance_table(differance_table):
             item = "f(n)"
         else:
             item = f"Δ^({count})f(n)"
-        print(f"{item:>{max_len}}", end="")
+        print(f"{item:<{max_len}}", end="      ")
         for item in row:
-            print(f"{item:>{max_len}}", end="")
+            print(f"{item:<{max_len}}", end="")
         print()
         count += 1
 

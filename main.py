b27eb798e8ad72d87d31459d21ab80da0e7bc5e8
Nate Stott <nathanstott01@gmail.com>
2024-01-17 22:40:36 -0700

    made better

--- main.py
+++ main.py
@@ -1,27 +1,42 @@
 
 def main():
-    print_differance_table(get_differance_table(lambda n : n ** 7, 11))
+    function = lambda n : (n ** 3) * (n + 1) * (n + 2) * (n + 3)
+    left = -10
+    right = 10
+    down = 10
+    print_differance_table(get_differance_table(function, left, right, down))
 
 
-def get_differance_table(function, columns):
+def get_differance_table(function, left=0, right=10, down=10):
+    right += 1
+    down += 2
     differance_table = [[], []]
-    for n in range(0, columns):
+    for n in range(left, right):
         differance_table[0].append(n)
         differance_table[1].append(function(n))
-    count = 0
-    for i in range(2, columns - 1):
+
+    for i in range(2, down):
         differance_table.append([])
-        for n in range(0, columns - 1 - count):
-            differance_table[i].append(differance_table[i - 1][n + 1] - differance_table[i - 1][n])
-        count += 1
+        for j in range(0, len(differance_table[i - 1]) - 1):
+            differance_table[i].append(differance_table[i - 1][j + 1] - differance_table[i - 1][j])
+
     return differance_table
 
 def print_differance_table(differance_table):
-    max_len = max(len(str(item)) for row in differance_table for item in row)
+    max_len = max(max(max(max(len(str(item)) for row in differance_table for item in row), len("n")), len("f(n)")), len(f"Δ^({len(differance_table) + 1})f(n)"))
+    count = -1
     for row in differance_table:
+        if count == -1:
+            item = "n"
+        elif count == 0:
+            item = "f(n)"
+        else:
+            item = f"Δ^({count})f(n)"
+        print(f"{item:>{max_len}}", end="")
         for item in row:
-            print(f"{item:<{max_len}}", end="")
+            print(f"{item:>{max_len}}", end="")
         print()
+        count += 1
 
 
 if __name__ == '__main__':

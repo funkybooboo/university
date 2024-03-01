e70891f631bfa2749c298fa9b782c5ca2a47d4e1
Nate Stott <nate.stott@pm.me>
2024-02-29 17:46:54 -0800

    cleaned up

--- src/voting.py
+++ src/voting.py
@@ -4,14 +4,23 @@
 
 from copy import deepcopy
 from numpy import random
+from random import randint
 
 
 def main():
-    Election.simulation(10, 5, 1052, True)
-    Election.simulation(20, 5, 1052, True)
-    Election.simulation(30, 5, 1053)
-    Election.simulation(40, 5, 1053)
-    Election.simulation(50, 5, 1053)
+    seed = 1052
+    print("SIMULATION 1")
+    Election.simulation(10, 5, seed, True)
+    print("SIMULATION 2")
+    Election.simulation(20, 5, seed, True)
+    print("SIMULATION 3")
+    Election.simulation(30, 5, seed)
+    print("SIMULATION 4")
+    Election.simulation(40, 5, seed)
+    print("SIMULATION 5")
+    Election.simulation(50, 5, seed)
+    print("RANDOM SIMULATION")
+    Election.simulation(randint(2, 1000), randint(2, 10), seed)
 
 
 class Election:
@@ -23,15 +32,12 @@ class Election:
             self.connections = [0 for _ in range(self.election.voter_count)]
             self.ranked_candidates = []
             self.__create_connections()
+            self.__create_candidates()
             self.__rank_candidates()
             self.original_ranked_candidates = deepcopy(self.ranked_candidates)
 
         def __rank_candidates(self):
-            if len(self.ranked_candidates) == 0:
-                candidates = self.__create_candidates()
-            else:
-                candidates = self.ranked_candidates
-            self.ranked_candidates = sorted(candidates, key=lambda candidate: candidate["score"], reverse=True)
+            self.ranked_candidates = sorted(self.ranked_candidates, key=lambda candidate: candidate["score"], reverse=True)
             self.__set_candidate_place()
 
         def __create_connections(self):
@@ -55,7 +61,7 @@ class Election:
                     "place": 0
                 }
                 candidates.append(candidate)
-            return candidates
+            self.ranked_candidates = candidates
 
         def print_connections(self):
             connections = []
@@ -71,10 +77,16 @@ class Election:
             print(f"\tORDER: {[candidate['name'] for candidate in self.ranked_candidates]}")
 
         def cardinal_utility(self, winner_pk: int):
-            return abs(self.ranked_candidates[0]["score"] - self.ranked_candidates[winner_pk]["score"])
+            for candidate in self.ranked_candidates:
+                if candidate["pk"] == winner_pk:
+                    return abs(self.ranked_candidates[0]["score"] - candidate["score"])
+            return None
 
         def ordinal_utility(self, winner_pk: int):
-            return abs(self.ranked_candidates[0]["place"] - self.ranked_candidates[winner_pk]["place"])
+            for candidate in self.ranked_candidates:
+                if candidate["pk"] == winner_pk:
+                    return abs(self.ranked_candidates[0]["place"] - candidate["place"])
+            return None
 
         def vote(self):
             return self.ranked_candidates[0]["pk"]
@@ -213,29 +225,30 @@ class Election:
             return connection_votes, connection_count
 
         def remove_candidate(self, candidate_pk: int):
+            new_ranked_candidates = []
             for candidate in self.ranked_candidates:
-                if candidate["pk"] == candidate_pk:
-                    self.ranked_candidates.remove(candidate)
-                    break
+                if candidate["pk"] != candidate_pk:
+                    new_ranked_candidates.append(candidate)
+            self.ranked_candidates = new_ranked_candidates
             self.__rank_candidates()
 
-        def rest_ranked_candidates(self):
+        def rest_candidates(self):
             self.ranked_candidates = deepcopy(self.original_ranked_candidates)
 
     def __init__(self, voter_count: int, candidate_count: int, seed: int, verbose: bool = False):
         random.seed(seed)
         self.voter_count = voter_count
         self.candidate_count = candidate_count
+        self.original_candidate_count = candidate_count
         self.verbose = verbose
         self.voters = []
         for i in range(voter_count):
             self.voters.append(self.Voter(f"Voter{i}", i, self))
 
     def statistics(self):
-        if self.verbose:
-            print("STATISTICS")
-            self.__print_connections()
-            self.__print_rankings()
+        print("STATISTICS")
+        self.__print_connections()
+        self.__print_rankings()
 
     def __print_connections(self):
         print("CONNECTIONS")
@@ -250,6 +263,7 @@ class Election:
     def __remove_candidate(self, candidate_pk: int):
         for voter in self.voters:
             voter.remove_candidate(candidate_pk)
+        self.candidate_count -= 1
 
     def __voter_welfare(self, winner_pk: int, is_printing: bool = True):
         average_cu = 0
@@ -257,10 +271,10 @@ class Election:
         for voter in self.voters:
             cu = voter.cardinal_utility(winner_pk)
             ou = voter.ordinal_utility(winner_pk)
+            if cu is None or ou is None:
+                raise ValueError("Could not find winner in voter's rankings.")
             if is_printing and self.verbose:
-                print(f"{voter.name}")
-                print(f"\tCardinal Utility: {cu}")
-                print(f"\tOrdinal Utility: {ou}")
+                print(f"{voter.name} Cardinal Utility: {cu} Ordinal Utility: {ou}")
             average_cu += cu
             average_ou += ou
         average_cu /= len(self.voters)
@@ -268,19 +282,23 @@ class Election:
         return average_cu, average_ou
 
     def __reset_candidates(self):
+        self.candidate_count = self.original_candidate_count
         for voter in self.voters:
-            voter.rest_ranked_candidates()
+            voter.rest_candidates()
 
     def first_past_the_post_voting(self, is_social_network: bool):
         self.__reset_candidates()
         if is_social_network:
-            winner_pk, loser_pk = self.__social_network_vote()
+            winner_pk, loser_pk, minds_changed = self.__social_network_vote()
+            if self.verbose:
+                print(f"MINDS CHANGED: {minds_changed}")
         else:
             winner_pk, loser_pk = self.__vote()
-        print("WINNER:", winner_pk)
-        average_cu, average_ou = self.__voter_welfare(winner_pk)
-        print("AVERAGE CARDINAL UTILITY:", average_cu)
-        print("AVERAGE ORDINAL UTILITY:", average_ou)
+        if self.verbose:
+            print(f"WINNER: Candidate{winner_pk}")
+            average_cu, average_ou = self.__voter_welfare(winner_pk)
+            print("AVERAGE CARDINAL UTILITY:", average_cu)
+            print("AVERAGE ORDINAL UTILITY:", average_ou)
         return winner_pk
 
     def borda_voting(self):
@@ -291,17 +309,20 @@ class Election:
             for candidate in voter.ranked_candidates:
                 candidate_points[candidate["pk"]] += points[candidate["place"]]
         winner_pk = candidate_points.index(max(candidate_points))
-        print("WINNER:", winner_pk)
-        average_cu, average_ou = self.__voter_welfare(winner_pk)
-        print("AVERAGE CARDINAL UTILITY:", average_cu)
-        print("AVERAGE ORDINAL UTILITY:", average_ou)
+        if self.verbose:
+            print(f"WINNER: Candidate{winner_pk}")
+            average_cu, average_ou = self.__voter_welfare(winner_pk)
+            print("AVERAGE CARDINAL UTILITY:", average_cu)
+            print("AVERAGE ORDINAL UTILITY:", average_ou)
         return winner_pk
 
     def ranked_choice_voting(self, is_social_network: bool):
         self.__reset_candidates()
         for _ in range(self.candidate_count - 1):
             if is_social_network:
-                winner_pk, loser_pk = self.__social_network_vote()
+                winner_pk, loser_pk, minds_changed = self.__social_network_vote()
+                if self.verbose:
+                    print(f"MINDS CHANGED: {minds_changed}")
             else:
                 winner_pk, loser_pk = self.__vote()
             if self.verbose:
@@ -310,84 +331,104 @@ class Election:
                 average_cu, average_ou = self.__voter_welfare(winner_pk)
                 print("AVERAGE CARDINAL UTILITY:", average_cu)
                 print("AVERAGE ORDINAL UTILITY:", average_ou)
+                print()
             self.__remove_candidate(loser_pk)
         if is_social_network:
-            winner_pk, loser_pk = self.__social_network_vote()
+            winner_pk, loser_pk, minds_changed = self.__social_network_vote()
+            if self.verbose:
+                print(f"MINDS CHANGED: {minds_changed}")
         else:
             winner_pk, loser_pk = self.__vote()
-        print(f"WINNER: Candidate{winner_pk}")
+        if self.verbose:
+            print(f"WINNER: Candidate{winner_pk}")
         self.__voter_welfare(winner_pk)
         return winner_pk
 
     def __vote(self):
-        votes = [0 for _ in range(self.candidate_count)]
+        votes = dict()
         for voter in self.voters:
-            votes[voter.vote()] += 1
-        winner_pk = votes.index(max(votes))
-        loser_pk = votes.index(min(votes))
+            vote_pk = voter.vote()
+            if vote_pk not in votes:
+                votes[vote_pk] = 1
+            else:
+                votes[vote_pk] += 1
+        winner_pk = max(votes, key=votes.get)
+        loser_pk = min(votes, key=votes.get)
         return winner_pk, loser_pk
 
     def __social_network_vote(self):
-        votes = [0 for _ in range(self.candidate_count)]
+        votes = dict()
+        minds_changed = 0
         for voter in self.voters:
-            votes[voter.social_network_vote()] += 1
-        winner_pk = votes.index(max(votes))
-        loser_pk = votes.index(min(votes))
-        return winner_pk, loser_pk
+            nv = voter.social_network_vote()
+            v = voter.vote()
+            if nv not in votes:
+                votes[nv] = 1
+            else:
+                votes[nv] += 1
+            if nv != v:
+                minds_changed += 1
+        winner_pk = max(votes, key=votes.get)
+        loser_pk = min(votes, key=votes.get)
+        return winner_pk, loser_pk, minds_changed
 
     @staticmethod
     def simulation(voter_count: int, candidate_count: int, seed: int, verbose: bool = False):
         print("*" * 50)
+        print()
         print(f"VOTER COUNT: {voter_count}")
         print(f"CANDIDATE COUNT: {candidate_count}")
         print(f"SEED: {seed}")
         print(f"VERBOSE: {verbose}")
-        print()
+        if verbose:
+            print()
         election = Election(voter_count, candidate_count, seed, verbose)
-        print()
-        election.statistics()
-        print()
-        print("FIRST PAST THE POST VOTING")
+        if verbose:
+            print()
+            election.statistics()
+            print()
+            print("FIRST PAST THE POST VOTING")
         winner_pk_fptp = election.first_past_the_post_voting(False)
-        print()
-        print("RANKED CHOICE VOTING")
+        if verbose:
+            print()
+            print("RANKED CHOICE VOTING")
         winner_pk_rc = election.ranked_choice_voting(False)
-        print()
-        print("FIRST PAST THE POST VOTING SOCIAL NETWORK")
+        if verbose:
+            print()
+            print("FIRST PAST THE POST VOTING SOCIAL NETWORK")
         winner_pk_fptpn = election.first_past_the_post_voting(True)
-        print()
-        print("RANKED CHOICE VOTING SOCIAL NETWORK")
+        if verbose:
+            print()
+            print("RANKED CHOICE VOTING SOCIAL NETWORK")
         winner_pk_rcn = election.ranked_choice_voting(True)
-        print()
-        print("BORDA VOTING")
+        if verbose:
+            print()
+            print("BORDA VOTING")
         winner_pk_b = election.borda_voting()
         print()
         print("--Comparing Voting Methods--")
-        print(f"FIRST PAST THE POST VOTING WINNER: Candidate{winner_pk_fptp}")
-        print(f"RANKED CHOICE VOTING WINNER: Candidate{winner_pk_rc}")
-        print(f"FIRST PAST THE POST VOTING SOCIAL NETWORK WINNER: Candidate{winner_pk_fptpn}")
-        print(f"RANKED CHOICE VOTING SOCIAL NETWORK WINNER: Candidate{winner_pk_rcn}")
-        print(f"BORDA VOTING WINNER: Candidate{winner_pk_b}")
         average_cu_fptp, average_ou_fptp = election.__voter_welfare(winner_pk_fptp, False)
         average_cu_rc, average_ou_rc = election.__voter_welfare(winner_pk_rc, False,)
         average_cu_fptpn, average_ou_fptpn = election.__voter_welfare(winner_pk_fptpn, False)
         average_cu_rcn, average_ou_rcn = election.__voter_welfare(winner_pk_rcn, False)
         average_cu_b, average_ou_b = election.__voter_welfare(winner_pk_b, False)
+        print(f"FIRST PAST THE POST VOTING WINNER: Candidate{winner_pk_fptp}")
+        print(f"\tAVERAGE CARDINAL UTILITY: {average_cu_fptp}")
+        print(f"\tAVERAGE ORDINAL UTILITY: {average_ou_fptp}")
+        print(f"RANKED CHOICE VOTING WINNER: Candidate{winner_pk_rc}")
+        print(f"\tAVERAGE CARDINAL UTILITY: {average_cu_rc}")
+        print(f"\tAVERAGE ORDINAL UTILITY: {average_ou_rc}")
+        print(f"FIRST PAST THE POST VOTING SOCIAL NETWORK WINNER: Candidate{winner_pk_fptpn}")
+        print(f"\tAVERAGE CARDINAL UTILITY: {average_cu_fptpn}")
+        print(f"\tAVERAGE ORDINAL UTILITY: {average_ou_fptpn}")
+        print(f"RANKED CHOICE VOTING SOCIAL NETWORK WINNER: Candidate{winner_pk_rcn}")
+        print(f"\tAVERAGE CARDINAL UTILITY: {average_cu_rcn}")
+        print(f"\tAVERAGE ORDINAL UTILITY: {average_ou_rcn}")
+        print(f"BORDA VOTING WINNER: Candidate{winner_pk_b}")
+        print(f"\tAVERAGE CARDINAL UTILITY: {average_cu_b}")
+        print(f"\tAVERAGE ORDINAL UTILITY: {average_ou_b}")
         print()
-        print("AVERAGE CARDINAL UTILITY")
-        print(f"FIRST PAST THE POST VOTING: {average_cu_fptp}")
-        print(f"RANKED CHOICE VOTING: {average_cu_rc}")
-        print(f"FIRST PAST THE POST VOTING SOCIAL NETWORK: {average_cu_fptpn}")
-        print(f"RANKED CHOICE VOTING SOCIAL NETWORK: {average_cu_rcn}")
-        print(f"BORDA VOTING: {average_cu_b}")
-        print()
-        print("AVERAGE ORDINAL UTILITY")
-        print(f"FIRST PAST THE POST VOTING: {average_ou_fptp}")
-        print(f"RANKED CHOICE VOTING: {average_ou_rc}")
-        print(f"FIRST PAST THE POST VOTING SOCIAL NETWORK: {average_ou_fptpn}")
-        print(f"RANKED CHOICE VOTING SOCIAL NETWORK: {average_ou_rcn}")
-        print(f"BORDA VOTING: {average_ou_b}")
-        print()
+        print("*" * 50)
 
 
 if __name__ == '__main__':

34d09a6fc4cb9aea5eee7e5378fb6f628732a0a5
Nate Stott <nate.stott@pm.me>
2024-04-04 08:12:47 -0600

    finished inisal implmentation

---
+++ src/HexGame.java
@@ -0,0 +1,296 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class HexGame {
+
+    private static final String ANSI_RESET = "\u001B[0m";
+    private static final String ANSI_BLACK = "\u001B[30m";
+    private static final String ANSI_RED = "\u001B[31m";
+    private static final String ANSI_GREEN = "\u001B[32m";
+    private static final String ANSI_YELLOW = "\u001B[33m";
+    private static final String ANSI_BLUE = "\u001B[34m";
+    private static final String ANSI_PURPLE = "\u001B[35m";
+    private static final String ANSI_CYAN = "\u001B[36m";
+    private static final String ANSI_WHITE = "\u001B[37m";
+
+    private final int xSize;
+    private final int ySize;
+
+    private final int cellCount;
+
+    private final String[] board;
+
+    private final UnionFind unionBlue;
+
+    private final UnionFind unionRed;
+
+    private final int TOP;
+    private final int BOTTOM;
+    private final int LEFT;
+    private final int RIGHT;
+
+    public HexGame() {
+        this.xSize = 11;
+        this.ySize = 11;
+        cellCount = (xSize * ySize) + 4;
+        board = new String[cellCount];
+        for (int i = 0; i < cellCount; i++) {
+            board[i] = ANSI_WHITE + "0" + ANSI_RESET;
+        }
+        TOP = 0;
+        LEFT = cellCount + 1;
+        RIGHT = cellCount + 2;
+        BOTTOM = cellCount + 3;
+        unionBlue = new UnionFind(cellCount);
+        unionRed = new UnionFind(cellCount);
+    }
+
+    public HexGame(int xSize, int ySize) {
+        this.xSize = xSize;
+        this.ySize = ySize;
+        cellCount = (xSize * ySize) + 4;
+        board = new String[cellCount];
+        for (int i = 0; i < cellCount; i++) {
+            board[i] = ANSI_WHITE + "0" + ANSI_RESET;
+        }
+        TOP = 0;
+        LEFT = cellCount + 1;
+        RIGHT = cellCount + 2;
+        BOTTOM = cellCount + 3;
+        unionBlue = new UnionFind(cellCount);
+        unionRed = new UnionFind(cellCount);
+    }
+
+    private void getMoves(String filePath, List<Integer> blueMoves, List<Integer> redMoves) {
+        try {
+            Scanner scanner = new Scanner(new File(filePath));
+            for (int i = 0; scanner.hasNextLine(); i++) {
+                boolean isBlue = i % 2 == 0;
+                int item = scanner.nextInt();
+                if (isBlue) blueMoves.add(item);
+                else redMoves.add(item);
+            }
+        }
+        catch (FileNotFoundException e) {
+            System.out.println(e.toString());
+        }
+    }
+
+    public void play(String filePath) {
+        List<Integer> blueMoves = new ArrayList<>();
+        List<Integer> redMoves = new ArrayList<>();
+        getMoves(filePath, blueMoves, redMoves);
+        if (blueMoves.size() != redMoves.size()) {
+            System.out.println("They need to play the same number of moves");
+            return;
+        }
+        for (int i = 0; i < blueMoves.size(); i++) {
+            int blueMove = blueMoves.get(i);
+            if (move(blueMove, unionBlue, unionRed)) {
+                board[getIndex(blueMove)] = ANSI_BLUE + "B" + ANSI_RESET;
+                if (isBlueWinner()) {
+                    printWinner("Blue", i);
+                    return;
+                }
+            }
+            else {
+                System.out.println("Blue invalid move: " + blueMove);
+            }
+
+            int redMove = redMoves.get(i);
+            if (move(redMove, unionRed, unionBlue)) {
+                board[getIndex(redMove)] = ANSI_RED + "R" + ANSI_RESET;
+                if (isRedWinner()) {
+                    printWinner("Red", i);
+                    return;
+                }
+            }
+            else {
+                System.out.println("Red invalid move: " + redMove);
+            }
+        }
+        System.out.println("Ran out of moves and no winner");
+    }
+
+    private void printWinner(String title, int attempts) {
+        System.out.println("---------> " + title +" has won after " + attempts + " attempted moves! Here is the final board.");
+        printBoard();
+    }
+
+    private boolean isBlueWinner() {
+        return unionBlue.isSameGroup(RIGHT, LEFT);
+    }
+
+    private boolean isRedWinner() {
+        return unionRed.isSameGroup(TOP, BOTTOM);
+    }
+
+    private boolean isTaken(int item) {
+        boolean isBlueGroup = unionBlue.find(item) != -1;
+        boolean isRedGroup = unionRed.find(item) != -1;
+        return isBlueGroup || isRedGroup;
+    }
+
+    private boolean move(int item, UnionFind union, UnionFind otherUnion) {
+        if (isTaken(item)) return false;
+        int[] neighbors = getNeighbors(item);
+        for (int neighbor : neighbors) {
+            boolean isOtherGroup = otherUnion.find(neighbor) != -1;
+            if (!isOtherGroup) union.union(neighbor, item);
+        }
+        return true;
+    }
+
+    private int getItem(int index) {
+        return index + 1;
+    }
+
+    private int getIndex(int item) {
+        return item - 1;
+    }
+
+    private boolean isRightEdge(int item) {
+        return item % ySize == 0;
+    }
+
+    private boolean isLeftEdge(int item) {
+        return (item - 1) % ySize == 0;
+    }
+
+    private boolean isTopEdge(int item) {
+        return item <= xSize;
+    }
+
+    private boolean isBottomEdge(int item) {
+        return item >= cellCount - xSize;
+    }
+
+    private boolean isEdge(int item) {
+        return isLeftEdge(item) || isRightEdge(item) || isTopEdge(item) || isBottomEdge(item);
+    }
+
+    private int[] getNeighbors(int item) {
+        int[] neighbors = new int[6];
+        if (isEdge(item)) {
+            if (isTopEdge(item)) {
+                neighbors = new int[]{
+                    item - 1,
+                    item + 1,
+                    TOP,
+                    TOP,
+                    item + 10,
+                    item + 11
+                };
+            }
+            else if (isBottomEdge(item)) {
+                neighbors = new int[]{
+                    item - 1,
+                    item + 1,
+                    item - 11,
+                    item - 10,
+                    BOTTOM,
+                    BOTTOM
+                };
+            }
+            else if (isLeftEdge(item)) {
+                neighbors = new int[]{
+                    LEFT,
+                    item + 1,
+                    item - 11,
+                    item - 10,
+                    LEFT,
+                    item + 11
+                };
+            }
+            else if (isRightEdge(item)) {
+                neighbors = new int[]{
+                    item - 1,
+                    RIGHT,
+                    item - 11,
+                    RIGHT,
+                    item + 10,
+                    item + 11
+                };
+            }
+            else if (isLeftEdge(item) && isTopEdge(item)) {
+                neighbors = new int[]{
+                    LEFT,
+                    item + 1,
+                    TOP,
+                    TOP,
+                    LEFT,
+                    item + 11
+                };
+            }
+            else if (isRightEdge(item) && isTopEdge(item)) {
+                neighbors = new int[]{
+                    item - 1,
+                    RIGHT,
+                    TOP,
+                    TOP,
+                    item + 10,
+                    item + 11
+                };
+            }
+            else if (isLeftEdge(item) && isBottomEdge(item)) {
+                neighbors = new int[]{
+                    LEFT,
+                    item + 1,
+                    item - 11,
+                    item - 10,
+                    BOTTOM,
+                    BOTTOM
+                };
+            }
+            else if (isRightEdge(item) && isBottomEdge(item)) {
+                neighbors = new int[]{
+                    item - 1,
+                    RIGHT,
+                    item - 11,
+                    RIGHT,
+                    BOTTOM,
+                    BOTTOM
+                };
+            }
+        }
+        else {
+            neighbors = new int[]{
+                item - 1,
+                item + 1,
+                item - 11,
+                item - 10,
+                item + 10,
+                item + 11
+            };
+        }
+        return neighbors;
+    }
+
+    public void printBoard() {
+        StringBuilder spaces = new StringBuilder();
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < cellCount; i++) {
+            if ((i+1) % xSize == 0) {
+                spaces.append(" ");
+                stringBuilder.append("\n").append(spaces);
+            }
+            String cell = board[i];
+            stringBuilder.append(cell).append(" ");
+
+        }
+        System.out.println(stringBuilder);
+    }
+
+    public static void main(String[] args) {
+        HexGame hexGame = new HexGame();
+        for (int i = 1; i < 3; i++) {
+            String filePath = "../data/moves"+i+".txt";
+            System.out.println("Playing: " + filePath);
+            hexGame.play(filePath);
+            System.out.println();
+        }
+    }
+}
---
+++ src/UnionFind.java
@@ -0,0 +1,143 @@
+import java.util.Random;
+
+public class UnionFind {
+
+    // item : index | group : stored or -size if root
+    // union-by-size
+
+    int[] groups;
+
+    public UnionFind(int elements) {
+        groups = new int[elements];
+        for (int i = 0; i < elements; i++) {
+            groups[i] = -1;
+        }
+    }
+
+    public void union(int indexA, int indexB) {
+        if (indexA < 0 || indexA > groups.length-1) return;
+        if (indexB < 0 || indexB > groups.length-1) return;
+        int groupIndexA = find(indexA);
+        int groupIndexB = find(indexB);
+        if (groupIndexA == groupIndexB) return;
+        if (groups[groupIndexA] <= groups[groupIndexB]) {
+            groups[groupIndexB] = groupIndexA;
+            groups[groupIndexA] -= 1;
+        } else {
+            groups[groupIndexA] = groupIndexB;
+            groups[groupIndexB] -= 1;
+        }
+    }
+
+    public Integer find(int indexA) {
+        if (indexA < 0 || indexA > groups.length-1) return null;
+        if (groups[indexA] < 0) {
+            return indexA;
+        }
+        groups[indexA] = find(groups[indexA]);
+        return groups[indexA];
+    }
+
+    public boolean isSameGroup(int indexA, int indexB) {
+        if (indexA < 0 || indexA > groups.length-1) return false;
+        if (indexB < 0 || indexB > groups.length-1) return false;
+        if (groups[indexA] < 0 || groups[indexB] < 0) return false;
+        int groupIndexA = find(indexA);
+        int groupIndexB = find(indexB);
+        return groupIndexA == groupIndexB;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(" [ item : group ] \n");
+        for (int i = 0; i < groups.length; i++) {
+            stringBuilder.append(" [ ").append(i).append(" : ").append(groups[i]).append(" ] ").append("\n");
+        }
+        return stringBuilder.toString();
+    }
+
+    public static void main(String[] args) {
+        test();
+    }
+
+    private static void test() {
+
+        System.out.println("Union Two Small Groups Test");
+        int data = 5;
+        UnionFind unionFind = new UnionFind(data);
+        System.out.println(unionFind);
+        unionFind.union(0, 1);
+        System.out.println("Union("+0+", "+1+")");
+        System.out.println(unionFind);
+        unionFind.union(1, 2);
+        System.out.println("Union("+1+", "+2+")");
+        System.out.println(unionFind);
+        unionFind.union(3, 4);
+        System.out.println("Union("+3+", "+4+")");
+        System.out.println(unionFind);
+        unionFind.union(4, 5);
+        System.out.println("Union("+4+", "+5+")");
+        System.out.println(unionFind);
+        System.out.println("Small groups made");
+        System.out.println(unionFind);
+        unionFind.union(2, 5);
+        System.out.println("Union("+2+", "+5+")");
+        System.out.println("Union the small groups to make one larger group");
+        System.out.println(unionFind);
+        System.out.println("---------------\n");
+
+        System.out.println("Union two items together that are already in the same group");
+        System.out.println(unionFind);
+        unionFind.union(2, 5);
+        System.out.println("Union("+2+", "+5+")");
+        System.out.println(unionFind);
+        System.out.println("---------------\n");
+
+        System.out.println("Show path compression");
+        System.out.println(unionFind);
+        unionFind.find(4);
+        System.out.println("Find("+4+")");
+        System.out.println(unionFind);
+        System.out.println("---------------\n");
+
+        System.out.println("Union and find items out of bounds");
+        unionFind = new UnionFind(data);
+        System.out.println(unionFind);
+        unionFind.find(-1);
+        System.out.println("Find("+-1+")");
+        System.out.println(unionFind);
+        unionFind.find(6);
+        System.out.println("Find("+6+")");
+        System.out.println(unionFind);
+        unionFind.union(-1, 4);
+        System.out.println("Union("+-1+", "+4+")");
+        System.out.println(unionFind);
+        unionFind.union(1, 6);
+        System.out.println("Union("+1+", "+6+")");
+        System.out.println(unionFind);
+        System.out.println("---------------\n");
+
+        System.out.println("Union the same item");
+        System.out.println(unionFind);
+        unionFind.union(4, 4);
+        System.out.println("Union("+4+", "+4+")");
+        System.out.println(unionFind);
+        System.out.println("---------------\n");
+
+        System.out.println("Random Union Test");
+        int size = 15;
+        unionFind = new UnionFind(data);
+        System.out.println(unionFind);
+        Random random = new Random();
+        for (int i = 0; i < size; i++) {
+            int a = random.nextInt(size);
+            int b = random.nextInt(size);
+            System.out.println("Union("+a+", "+b+")");
+            unionFind.union(a, b);
+            System.out.println(unionFind);
+        }
+        System.out.println("---------------\n");
+    }
+
+}

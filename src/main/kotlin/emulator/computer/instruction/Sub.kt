9ba90a6ea6923542ac7e95be957ddd33a80db3d1
Nate Stott <nate.stott@pm.me>
2024-07-27 18:38:08 -0600

    can run substaction

--- src/main/kotlin/emulator/computer/instruction/Sub.kt
+++ src/main/kotlin/emulator/computer/instruction/Sub.kt
@@ -1,7 +1,5 @@
 package com.natestott.emulator.computer.instruction
 
-import com.natestott.emulator.computer.byteArrayToInt
-import com.natestott.emulator.computer.intToByteArray
 import com.natestott.emulator.computer.memory.register.RManager.r
 import com.natestott.emulator.logger.LoggerManager.logger
 import com.natestott.emulator.logger.Logger.Level
@@ -11,24 +9,20 @@ class Sub(
 ) : Instruction(nibbles) {
     override fun performOperation() {
         val rxIndex = nibbles[0].toInt()
-        val rx = r[rxIndex]
-
         val ryIndex = nibbles[1].toInt()
-        val ry = r[ryIndex]
-
         val rzIndex = nibbles[2].toInt()
+
+        val rx = r[rxIndex]
+        val ry = r[ryIndex]
         val rz = r[rzIndex]
 
-        val int1 = byteArrayToInt(rx.read())
-        val int2 = byteArrayToInt(ry.read())
-        val result = int1 - int2
-        val resultBytes = intToByteArray(result)
+        val rxValue = rx.read()[0].toInt()
+        val ryValue = ry.read()[0].toInt()
 
-        logger.log(Level.INFO, "Performing Subtraction Operation:")
-        logger.log(Level.INFO, "Subtracting value in R$ryIndex (${int2.toUShort()}) from value in R$rxIndex (${int1.toUShort()})")
-        logger.log(Level.INFO, "Result of subtraction: $result")
-        logger.log(Level.INFO, "Storing result in R$rzIndex")
+        val result = (rxValue - ryValue).toByte()
 
-        rz.write(resultBytes)
+        rz.write(byteArrayOf(result))
+
+        logger.log(Level.INFO, "Added values from r$rxIndex and r$ryIndex. Result: $result stored in r$rzIndex.")
     }
 }

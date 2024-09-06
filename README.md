364d80fdb8e35d73e78ff0006a2061da3f4d0d16
Nate Stott <nate.stott@pm.me>
2024-09-06 17:56:56 -0600

    clean up a bit

--- README.md
+++ README.md
@@ -1,14 +1,25 @@
 # CS5060_Program1
 
 ## Team Members
-- Ann Marie
+- Ann Marie Humble
 - Brighton Ellis
 - Nate Stott
 - Madison Patch
 
 ## Optimal Stopping Results
 ### Part One
-[Here's what we figured out]
+Our algorithm (meaning Dr. Harper's algorithm, thank you Dr. Harper) 
+consistently found the most optimal candidates 
+after searching around 37% of the candidate pool. The best strategy 
+for choosing a candidate, then, would be to interview and dismiss
+the first 37% of candidates, and then accept the next best candidate after that.
+However, this strategy still only finds the optimal candidate about
+37% percent of the time. Which isn't _comforting_. But it's the best we can do.
+
+A side note, it's kind of cool that we get the same percentage for the
+likelihood of finding the optimal candidate, as for the amount of candidates 
+to go through before making a choice. We're not totally sure why that is.
+
 ### Part Two
 ### Part Three
 
--- src/explore_stopping.py
+++ src/explore_stopping.py
@@ -3,55 +3,56 @@ import matplotlib.pyplot as plt
 
 # Part 1 - Brute force
 
-number_of_candidates = 50
-number_of_experiments = 1000
+NUMBER_OF_CANDIDATES = 50
+NUMBER_OF_EXPERIMENTS = 1000
 
-position_was_optimal_count = {}
-optimal_solution_found_count = {}
+def main():
+    # Initialize dictionaries with zero values
+    position_was_optimal_count = {str(i): 0 for i in range(1, NUMBER_OF_CANDIDATES + 1)}
+    optimal_solution_found_count = {str(i): 0 for i in range(1, NUMBER_OF_CANDIDATES + 1)}
 
-# This just creates a zero dictionary
-for i in range(1, number_of_candidates + 1):
-    position_was_optimal_count[str(i)] = 0
-    optimal_solution_found_count[str(i)] = 0
+    for experiment in range(NUMBER_OF_EXPERIMENTS):
+        candidates = random.sample(range(1000), NUMBER_OF_CANDIDATES)
+        optimal_candidate = max(candidates)
+        print("---------")
+        print(f"experiment {experiment}")
+        print(f"optimal_candidate: {optimal_candidate}")
+        print(f"candidates: {candidates}")
+        print("---------")
 
-for experiment in range(number_of_experiments):
-    candidates = random.sample(range(1000), number_of_candidates)
-    optimal_candidate = max(candidates)
+        # skip the start because you're not going stop on the first person
+        for i in range(1, NUMBER_OF_CANDIDATES + 1):
+            # exclude the end person because that's not an optimal stopping (you went threw the whole thing)
+            for candidate in candidates[i:-1]:
+                # if they are better than what we have seen so far
+                if candidate > max(candidates[0:i]):
+                    # number of times that position was best threw all the experiments at any point in time
+                    position_was_optimal_count[str(i)] += 1
+                    if candidate == optimal_candidate:
+                        # record that we have found /the optimal/ solution
+                        optimal_solution_found_count[str(i)] += 1 / NUMBER_OF_EXPERIMENTS
+                    break
+
+    positions, times_optimal = zip(*optimal_solution_found_count.items())
+
     print("---------")
-    print(f"experiment {experiment}")
-    print(f"optimal_candidate: {optimal_candidate}")
-    print(f"candidates: {candidates}")
+    print("final results")
+    print(f"positions: {positions}")
+    print(f"times_optimal: {times_optimal}")
     print("---------")
 
-    # skip the start because you're not going stop on the first person
-    for i in range(1, number_of_candidates + 1):
-        # exclude the end person because that's not an optimal stopping (you went threw the whole thing)
-        for candidate in candidates[i:-1]:
-            # if they are better than what we have seen so far
-            if candidate > max(candidates[0:i]):
-                # number of times that position was best threw all the experiments at any point in time
-                position_was_optimal_count[str(i)] += 1
-                if candidate == optimal_candidate:
-                    # record that we have found /the optimal/ solution
-                    optimal_solution_found_count[str(i)] += 1 / number_of_experiments
-                break
+    plt.plot(positions, times_optimal)
+    plt.show()
 
-positions, times_optimal = zip(*optimal_solution_found_count.items())
+    # Part 2 - Relax Distribution
 
-print("---------")
-print("final results")
-print(f"positions: {positions}")
-print(f"times_optimal: {times_optimal}")
-print("---------")
+    # homepage.divms.uiowa.edu/~mbognar/applets/beta.html
+    # play around with the distribution
 
-plt.plot(positions, times_optimal)
-plt.show()
+    # Part 3 - Investment Decisions
 
-# Part 2 - Relax Distribution
+    # what happens to the optimal stop if you have to pay to check
 
-# homepage.divms.uiowa.edu/~mbognar/applets/beta.html
-# play around with the distribution
 
-# Part 3 - Investment Decisions
-
-# what happens to the optimal stop if you have to pay to check
+if __name__ == "__main__":
+    main()

6373e6e50db8442e6b6a4f600813637e96098e63
Nate Stott <nate.stott@pm.me>
2024-10-04 07:56:31 -0600

    set up

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,20 @@
+#include "sortutils.hpp"
+
+#include <iostream>
+
 int main()
 {
-    
+
+}
+
+template <typename T>
+std::string getCollectionStats(std::string& collectionTitle, T& collection)
+{
+    std::string stats = "--- " + collectionTitle + " Performance ---";
+
+    // TODO add Sequential to stats
+
+    // TODO add Parallel to stats
+
+    return stats;
 }
---
+++ src/sortutils.cpp
@@ -0,0 +1,31 @@
+#include "sortutils.hpp"
+
+#include <cstdint>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+void initializeRawArrayFromStdArray(const SourceArray& source, int dest[])
+{
+
+}
+
+void organPipeStdArray(SourceArray& data)
+{
+
+}
+
+void evaluateRawArray(const SourceArray& random, const SourceArray& sorted, const SourceArray& reversed, const SourceArray& organPipe, const SourceArray& rotated)
+{
+
+}
+
+void evaluateStdArray(const SourceArray& random, const SourceArray& sorted, const SourceArray& reversed, const SourceArray& organPipe, const SourceArray& rotated)
+{
+
+}
+
+void evaluateStdVector(const SourceArray& random, const SourceArray& sorted, const SourceArray& reversed, const SourceArray& organPipe, const SourceArray& rotated)
+{
+
+}

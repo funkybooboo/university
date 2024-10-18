b1d1b215030a6e578174d0d320cd84328da04064
Nate Stott <nate.stott@pm.me>
2024-10-18 14:40:18 -0600

    fix formatting

--- src/WordTree/WordTree.hpp
+++ src/WordTree/WordTree.hpp
@@ -12,20 +12,30 @@ class WordTree
 {
   public:
     WordTree();
+
     void add(const std::string& word);
+
     [[nodiscard]] bool find(const std::string& word) const;
+
     [[nodiscard]] std::vector<std::string> predict(const std::string& partial, std::uint8_t howMany) const;
+
     [[nodiscard]] std::size_t size() const;
 
   private:
     std::uint32_t m_size;
+
     std::optional<std::shared_ptr<TreeNode>> m_root;
 
     static void tolower(std::string& word);
+
     static bool isAlphaOnly(const std::string& word);
+
     void addHelper(const std::shared_ptr<TreeNode>& node, const std::string& word, size_t index);
+
     [[nodiscard]] bool findHelper(const std::shared_ptr<TreeNode>& node, const std::string& word, size_t index) const;
+
     [[nodiscard]] std::optional<std::shared_ptr<TreeNode>> traverseToEndOfPartial(const std::string& lowerPartial) const;
+
     static void collectPredictions(
         const std::shared_ptr<TreeNode>& currentNode,
         const std::string& lowerPartial,
--- src/main.cpp
+++ src/main.cpp
@@ -61,6 +61,7 @@ std::vector<std::string> split(const std::string& s, const char delim)
 
 void render(const std::shared_ptr<WordTree>& wordTree)
 {
+
 }
 
 int main()

07cd8ec9301da3fc60a7266e92c2ac2316dd28dc
Nate Stott <nate.stott@pm.me>
2024-11-17 13:06:26 -0700

    fix main

--- src/TaskFIFO.java
+++ src/TaskFIFO.java
@@ -1,18 +1,44 @@
 import java.util.LinkedList;
 import java.util.Queue;
 
+/**
+ * The TaskFIFO class simulates the First-In-First-Out (FIFO) page replacement algorithm.
+ * It manages the sequence of page references and tracks the number of page faults that occur
+ * when trying to fit the pages into a fixed number of memory frames.
+ * <p>
+ * The FIFO algorithm removes the oldest page in memory when a new page needs to be loaded,
+ * provided there is no space left in memory.
+ */
 public class TaskFIFO implements Runnable {
 
+    /**
+     * The sequence of page references to be processed.
+     */
     private final int[] sequence;
-    private final int maxMemoryFrames;
-    private final int maxPageReference;
-    private final int[] pageFaults; // output
 
-    /*
-     * sequence : (input) a randomly generated sequence of page references
-     * maxMemoryFrames : (input) the number of frames of memory available
-     * maxPageReference : (input) the maximum page reference possible in the sequence
-     * pageFaults : (output) an array used to record the number of page faults that occur each simulation of some number of frames.  Each call to the 'run' method of a task results in storing the number of page faults for the task using something like: pageFaults[maxMemoryFrames] = pageFaults (where pageFaults is the number of page faults your code detects).
+    /**
+     * The maximum number of memory frames available for storing pages.
+     */
+    private final int maxMemoryFrames;
+
+    /**
+     * The maximum possible page reference value in the sequence.
+     */
+    private final int maxPageReference;
+
+    /**
+     * An array used to record the number of page faults for each simulation with
+     * different numbers of memory frames.
+     */
+    private final int[] pageFaults;
+
+    /**
+     * Constructs a TaskFIFO object with the given parameters.
+     *
+     * @param sequence         An array of integers representing the sequence of page references.
+     * @param maxMemoryFrames  The maximum number of memory frames available.
+     * @param maxPageReference The maximum value of a page reference in the sequence.
+     * @param pageFaults       An array to store the number of page faults for each simulation.
      */
     protected TaskFIFO(int[] sequence, int maxMemoryFrames, int maxPageReference, int[] pageFaults) {
         this.sequence = sequence;
@@ -21,24 +47,35 @@ public class TaskFIFO implements Runnable {
         this.pageFaults = pageFaults;
     }
 
+    /**
+     * Executes the FIFO page replacement algorithm.
+     * This method simulates processing the sequence of page references and counts the number of page faults
+     * that occur for the given number of memory frames.
+     */
     @Override
     public void run() {
         Queue<Integer> memory = new LinkedList<>();
 
+        // Variable to track the number of page faults that occur during the simulation
         int pageFaultCount = 0;
 
+        // Loop through each page reference in the sequence
         for (int page : sequence) {
+            // If the page is not already in memory, it's a page fault
             if (!memory.contains(page)) {
                 pageFaultCount++;
 
+                // If memory is full, remove the oldest page
                 if (memory.size() == maxMemoryFrames) {
                     memory.poll();
                 }
 
+                // Add the new page to memory
                 memory.offer(page);
             }
         }
-        
+
+        // Record the number of page faults for the current number of memory frames
         pageFaults[maxMemoryFrames] = pageFaultCount;
     }
 }
--- src/TaskLRU.java
+++ src/TaskLRU.java
@@ -2,12 +2,44 @@ import java.util.LinkedList;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * The TaskLRU class simulates the Least Recently Used (LRU) page replacement algorithm.
+ * It tracks page references and determines the number of page faults that occur when
+ * attempting to fit pages into a fixed number of memory frames.
+ * <p>
+ * The LRU algorithm evicts the least recently used page when memory is full and a new page needs to be loaded.
+ */
 public class TaskLRU implements Runnable {
-    private final int[] sequence;
-    private final int maxMemoryFrames;
-    private final int maxPageReference;
-    private final int[] pageFaults; 
 
+    /**
+     * The sequence of page references to be processed.
+     */
+    private final int[] sequence;
+
+    /**
+     * The maximum number of memory frames available for storing pages.
+     */
+    private final int maxMemoryFrames;
+
+    /**
+     * The maximum possible page reference value in the sequence.
+     */
+    private final int maxPageReference;
+
+    /**
+     * An array used to store the number of page faults for each simulation with different
+     * numbers of memory frames.
+     */
+    private final int[] pageFaults;
+
+    /**
+     * Constructs a TaskLRU object with the given parameters.
+     *
+     * @param sequence         An array of integers representing the sequence of page references.
+     * @param maxMemoryFrames  The maximum number of memory frames available.
+     * @param maxPageReference The maximum value of a page reference in the sequence.
+     * @param pageFaults       An array to store the number of page faults for each simulation.
+     */
     protected TaskLRU(int[] sequence, int maxMemoryFrames, int maxPageReference, int[] pageFaults) {
         this.sequence = sequence;
         this.maxMemoryFrames = maxMemoryFrames;
@@ -15,17 +47,29 @@ public class TaskLRU implements Runnable {
         this.pageFaults = pageFaults;
     }
 
+    /**
+     * Executes the LRU page replacement algorithm.
+     * This method simulates processing the sequence of page references and counts the number of page faults
+     * that occur when trying to fit the pages into the given number of memory frames.
+     */
     @Override
     public void run() {
-        LinkedList<Integer> memory = new LinkedList<>();
+        // A linked list to maintain the order of pages in memory (for LRU)
+        LinkedList<Integer> memory pageFaultsFIFOs= new LinkedList<>();
+
+        // A set to track the pages currently in memory for fast look-up
         Set<Integer> memorySet = new HashSet<>();
 
+        // Variable to track the number of page faults that occur during the simulation
         int pageFaultCount = 0;
 
+        // Loop through each page reference in the sequence
         for (int page : sequence) {
+            // If the page is not already in memory, it's a page fault
             if (!memorySet.contains(page)) {
                 pageFaultCount++;
 
+                // If memory is full, remove the least recently used page
                 if (memory.size() == maxMemoryFrames) {
                     Integer leastRecentlyUsed = memory.pollFirst();
                     if (leastRecentlyUsed != null) {
@@ -33,14 +77,17 @@ public class TaskLRU implements Runnable {
                     }
                 }
 
+                // Add the new page to memory (end of the list, most recently used)
                 memory.addLast(page);
                 memorySet.add(page);
             } else {
+                // If the page is already in memory, move it to the end of the list (most recently used)
                 memory.remove((Integer) page);
                 memory.addLast(page);
             }
         }
 
+        // Store the number of page faults for the current number of memory frames
         pageFaults[maxMemoryFrames] = pageFaultCount;
     }
 }
